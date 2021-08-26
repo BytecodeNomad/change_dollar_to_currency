1c5be11b5baaf068a6c0a6f70abf31fba33cc47b
The Myth <57758228+TheMyth42@users.noreply.github.com>
2021-08-26 14:33:29 +0100

    second commit

--- transaction.py
+++ transaction.py
@@ -12,7 +12,11 @@ def automated_transaction():
         dollars = cents // 100
         #cents left after dollars converted
         cents_left = cents % 100
-    if cents:    
+    if cents:
+        #if cents is greater than 25 but less than 100 e.g 35
+        if cents >=25 and cents < 100:
+            quaters = 1
+            cents_left = cents % 25    
         if cents_left >= 25:
         #quaters in cents
             quaters = cents_left // 25
@@ -20,22 +24,31 @@ def automated_transaction():
             cents_left %= 25
         # if cents is greater than 25 but less than 100 e.g 99 cents
         if cents >= 25 and cents < 100:
-            
             quaters = cents // 25
         #cents left
             cents_left = cents % 25
-    if cents_left >= 10:
+    if cents:
+        if cents >=10 and cents < 25:
+            dimes = 1
+            cents_left = cents % 10
+        if cents_left >= 10:
         #dimes in cents
-        dimes = cents_left // 10
+            dimes = cents_left // 10
         #cents left
-        cents_left %= 10
-    if cents_left >=5:
+            cents_left %= 10
+    if cents:
+        if cents >= 5 and cents < 10:
+            nickels = 1
+            cents_left = cents % 5
+        if cents_left >=5:
         #nickels in cents
-        nickels = cents_left // 5
+            nickels = cents_left // 5
         #cents left
-        cents_left %= 5
+            cents_left %= 5
     if cents_left >= 1:
         #pennies in cents
         pennies = cents_left
+    if cents >= 1 and cents < 5:
+        pennies = cents
     print(f"{dollars}d {quaters}q {dimes}d {nickels}n {pennies}p")
 automated_transaction()
